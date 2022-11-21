d097d4365fb1bf5dd76da2d9effcfdd45c73b5b4
CarlosOliveira1988 <CarlosOliveira1988@users.noreply.github.com>
2022-11-21 19:05:16 -0300

    Adding a 'statistics_tab' to improve the front end organization

--- Home.py
+++ Home.py
@@ -26,34 +26,38 @@ if uploaded_file:
     statistics = StatisticsTables()
     statistics.set_file(uploaded_file)
 
-    mA_tab, kV_tab, ms_tab, failure_tab, warning_tab, exposition_tab, log_analysis_tab = st.tabs(
-        ["mA", "kV", "ms", "Falha", "Warning", "Exposição", "Análise de Logs"]
-    )
-        
-    with mA_tab:
-        mA_page = mA_StatisticsPage(statistics.mA_table, statistics.log_table)
-        mA_page.show_page()
-
-    with kV_tab:
-        kV_page = kV_StatisticsPage(statistics.kV_table, statistics.log_table)
-        kV_page.show_page()
-
-    with ms_tab:
-        ms_page = ms_StatisticsPage(statistics.ms_table, statistics.log_table)
-        ms_page.show_page()
-
-    with failure_tab:
-        failure_page = Failure_StatisticsPage(statistics.failure_table, statistics.log_table)
-        failure_page.show_page()
-
-    with warning_tab:
-        warning_page = Warning_StatisticsPage(statistics.warning_table, statistics.log_table)
-        warning_page.show_page()
-
-    with exposition_tab:
-        exposition_page = Exposition_StatisticsPage(statistics.exposition_table, statistics.log_table)
-        exposition_page.show_page()
+    log_analysis_tab, statistics_tab = st.tabs(["Análise de Logs", "Estatísticas"])
     
     with log_analysis_tab:
         logs_page = LogsPage(statistics.log_table, uploaded_file)
         logs_page.show_page()
+    
+    with statistics_tab:    
+    
+        mA_tab, kV_tab, ms_tab, failure_tab, warning_tab, exposition_tab = st.tabs(
+            ["mA", "kV", "ms", "Falha", "Warning", "Exposição"]
+        )
+    
+        with mA_tab:
+            mA_page = mA_StatisticsPage(statistics.mA_table, statistics.log_table)
+            mA_page.show_page()
+
+        with kV_tab:
+            kV_page = kV_StatisticsPage(statistics.kV_table, statistics.log_table)
+            kV_page.show_page()
+
+        with ms_tab:
+            ms_page = ms_StatisticsPage(statistics.ms_table, statistics.log_table)
+            ms_page.show_page()
+
+        with failure_tab:
+            failure_page = Failure_StatisticsPage(statistics.failure_table, statistics.log_table)
+            failure_page.show_page()
+
+        with warning_tab:
+            warning_page = Warning_StatisticsPage(statistics.warning_table, statistics.log_table)
+            warning_page.show_page()
+
+        with exposition_tab:
+            exposition_page = Exposition_StatisticsPage(statistics.exposition_table, statistics.log_table)
+            exposition_page.show_page()
